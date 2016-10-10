7889e4be870d275dafdaad2a0cf1edbd3129f040
Aaron Latham <drummerstrike@gmail.com>
2016-10-10 16:13:54 -0600

    api change

--- app/apiService.js
+++ app/apiService.js
@@ -8,7 +8,7 @@ angular.module("gameApp").service("apiService", function($http) {
 
     return $http({
       method: 'GET',
-      url: "http://api.giphy.com/v1/gifs/search?q=dancing+blastoise&api_key=dc6zaTOxFJmzC"
+      url: "https://api.giphy.com/v1/gifs/search?q=dancing+blastoise&api_key=dc6zaTOxFJmzC"
     }).then(function(response) {
       console.log(response);
       return response.data.data;
